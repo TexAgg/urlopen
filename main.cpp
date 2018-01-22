68763830d7bbd678ff4579a6fefc1ad85d897c05
Matt Gaikema <mgaikema1@protonmail.com>
2018-01-22 16:59:49 -0600

    for some reason argparse doen't properly parse -v

--- main.cpp
+++ main.cpp
@@ -14,43 +14,55 @@ int main(int argc, const char** argv)
 		NULL,
 		NULL,
 	};
+
+	bool print_version = false;
 	bool print_url = false;
 	char* c_fname = NULL;
 	argparse_option options[] = {
 		OPT_HELP(),
+		OPT_BOOLEAN('v', "version", &print_version, "Display the version and exit"),
 		OPT_GROUP("Options"),
-		OPT_BOOLEAN('p', "print-url", &print_url, "Display the url and quit"),
+		OPT_BOOLEAN('p', "print-url", &print_url, "Display the url and exit"),
 		OPT_END()
 	};
 
 	argparse ap;
 	argparse_init(&ap, options, usage, 0);
 	argc = argparse_parse(&ap, argc, argv);
-	std::cout << argc << std::endl;
 
-	int i;
-	if (argc != 0) 
+	std::cout << print_version << std::endl;
+	if (print_version)
 	{
-        for (i = 0; i < argc; i++) 
+		std::string version = URLOPEN_VERSION;
+		std::cout << version << std::endl;
+		//exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		int i;
+		if (argc != 0) 
 		{
-        	// Do nothing: just let i increment.
-        }
-    }
-	else
-	{
-		std::cerr << "No filename given!" << std::endl;
-		exit(EXIT_FAILURE);
-	}
+			for (i = 0; i < argc; i++) 
+			{
+				// Do nothing: just let i increment.
+			}
+		}
+		else
+		{
+			std::cerr << "No filename given!" << std::endl;
+			exit(EXIT_FAILURE);
+		}
 
-	std::string fname(argv[i-1]);
-	UrlFile uf(fname);
+		std::string fname(argv[i-1]);
+		UrlFile uf(fname);
 
-	if (print_url)
-	{
-		std::cout << uf.get_url() << std::endl;
-	}
-	else
-	{
-		uf.open_shortcut();
+		if (print_url)
+		{
+			std::cout << uf.get_url() << std::endl;
+		}
+		else
+		{
+			uf.open_shortcut();
+		}
 	}
 }
