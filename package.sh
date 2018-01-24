b571a0c5a3c76f90193a2fdf581ac39fa7167c9f
Matt Gaikema <mgaikema1@protonmail.com>
2018-01-24 15:26:42 -0600

    package by copying files to tempdir

--- package.sh
+++ package.sh
@@ -1,16 +1,19 @@
 # Creates a .deb package.
 
+TEMPDIR=urlopen_temp
+rm -rf $TEMPDIR
+mkdir $TEMPDIR
+cp -r -t $TEMPDIR CMakeLists.txt *.cpp *.hpp urlopen.1 urlopen.desktop img/ simpleini/ argparse/
+mkdir $TEMPDIR/_build
+
 # DON'T FORGET TO KEEP THIS UP TO DATE WITH urlopen.1 and CMakeLists.txt
 VERSION=3.0.0
-ARCHIVE=temp.tar
-FILES="img/urlopen_64x64.png urlopen.1 urlopen.desktop "
-tar --create --file=$ARCHIVE --exclude-vcs --exclude=_build/* \
-	--exclude=.vscode/ --exclude=*.tar --exclude=package.sh *
 
 # Create a temporary script with the preinstall script.
 TEMP=temp.sh
 # https://stackoverflow.com/a/23930212/5415895
 cat > $TEMP <<- EOM
+cd ${TEMPDIR}
 cd _build
 cmake ../
 make
@@ -19,19 +22,18 @@ EOM
 
 TEMP2=temp2.sh
 cat > $TEMP2 <<- EOM
-cd _build
-rm -rf *
+rm ${TEMPDIR}/_build/*
 EOM
 
-fpm -s tar -t deb -n "urlopen" --after-install $TEMP -v $VERSION \
+fpm -s dir -t deb -n "urlopen" --after-install $TEMP -v $VERSION \
 	--after-remove uninstall.sh \
 	--before-remove $TEMP2 \
 	--deb-build-depends build-essential \
 	--description "A tool for opening windows' internet shortcut files." \
 	--maintainer "mgaikema1@protonmail.com" \
 	--url "https://github.com/TexAgg/urlopen" \
-	$ARCHIVE
+	$TEMPDIR
 
-rm $ARCHIVE
 rm $TEMP
 rm $TEMP2
+rm -r $TEMPDIR
