9fd8164ee2b6346d3c2cea544b2f6d0c5040c350
Matt Gaikema <mgaikema1@protonmail.com>
2018-01-25 13:52:56 -0600

    create deb package

--- package.sh
+++ package.sh
@@ -1,38 +1,49 @@
 # Creates a .deb package.
 
-TEMPDIR=urlopen_temp
-rm -rf $TEMPDIR
-mkdir $TEMPDIR
-cp -r -t $TEMPDIR CMakeLists.txt *.cpp *.hpp urlopen.1 urlopen.desktop img/ simpleini/ argparse/
-mkdir $TEMPDIR/_build
+# Create temporary directory for packaging.
+TEMPDIR=$(mktemp -d -p .)
+
+mkdir -p $TEMPDIR/usr/local/bin
+cp -t $TEMPDIR/usr/local/bin _build/urlopen
+
+mkdir -p $TEMPDIR/usr/share/applications
+cp -t $TEMPDIR/usr/share/applications urlopen.desktop
+
+mkdir -p $TEMPDIR/usr/local/man/man1
+cp -t $TEMPDIR/usr/local/man/man1 urlopen.1
+
+mkdir -p $TEMPDIR/usr/share/pixmaps
+cp -t $TEMPDIR/usr/share/pixmaps img/urlopen_64x64.png
+
+mkdir -p $TEMPDIR/usr/share/icons/humanity/mimes/256
+cp -t $TEMPDIR/usr/share/icons/humanity/mimes/256 img/application-x-mswinurl.svg
 
 # DON'T FORGET TO KEEP THIS UP TO DATE WITH urlopen.1 and CMakeLists.txt
 VERSION=3.0.0
 
-# Create a temporary script with the preinstall script.
-TEMP=temp.sh
+# Create a temporary file with the preinstall script.
+TEMP=$(mktemp -p .)
 # https://stackoverflow.com/a/23930212/5415895
 cat > $TEMP <<- EOM
-cd ${TEMPDIR}
-cd _build
-cmake ../
-make
-sudo bash setup.sh
+echo "application/x-mswinurl=urlopen.desktop" >> $HOME/.local/share/applications/mimeapps.list
+echo "application/x-mswinurl url" | sudo tee -a /etc/mime.types
+sudo gtk-update-icon-cache /usr/share/icons/Humanity
 EOM
 
-TEMP2=temp2.sh
+# Create another temporary file with the post-remove script.
+TEMP2=$(mktemp -p .)
 cat > $TEMP2 <<- EOM
-rm ${TEMPDIR}/_build/*
+sed -i 's/\<application\/x-mswinurl=urlopen.desktop\>//g' $HOME/.local/share/applications/mimeapps.list
+sudo sed -i "s/\<application\/x-mswinurl url\>//g" /etc/mime.types
 EOM
 
 fpm -s dir -t deb -n "urlopen" --after-install $TEMP -v $VERSION \
-	--after-remove uninstall.sh \
-	--before-remove $TEMP2 \
+	--after-remove $TEMP2 \
 	--deb-build-depends build-essential \
 	--description "A tool for opening windows' internet shortcut files." \
 	--maintainer "mgaikema1@protonmail.com" \
 	--url "https://github.com/TexAgg/urlopen" \
-	$TEMPDIR
+	-C $TEMPDIR .
 
 rm $TEMP
 rm $TEMP2
